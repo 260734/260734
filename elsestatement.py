0c98b89a0a10b5958cd95adb1a359f0f4665d36b
260734 <sanskarsinha1999@gmail.com>
2021-04-20 23:46:37 +0530

    else Commit

---
+++ elsestatement.py
@@ -0,0 +1,11 @@
+num = 3
+if num == 1:
+    print("one")
+else:
+    if num == 2:
+        print("two")
+    else:
+        if num == 3:
+            print("three")
+        else:
+            print("Something Else")

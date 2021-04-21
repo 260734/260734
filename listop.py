96a1ffe9e39e0ec12d4b23f2bc043fb6510c94c5
260734 <sanskarsinha1999@gmail.com>
2021-04-21 23:18:45 +0530

    list operation Commit

---
+++ listop.py
@@ -0,0 +1,5 @@
+# It returns True if the item occurs one or more times in the list, and False if it doesn't.
+words = ["spam", "egg", "spam", "sausage"]
+print("spam" in words)
+print("egg" in words)
+print("tomato" in words)

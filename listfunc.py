c6ef8780452aff2b731bcac67e93df6fd29d1591
260734 <sanskarsinha1999@gmail.com>
2021-04-21 23:26:04 +0530

    list function Commit

---
+++ listfunc.py
@@ -0,0 +1,6 @@
+# The index method finds the first occurrence of a list item and returns its index.
+# If the item isn't in the list, it gives a ValueError.
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+print(letters.index('r'))
+print(letters.index('p'))
+print(letters.index('z'))

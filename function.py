e899cfc3eb0c466f7242ca5cb7c6220270dfda23
260734 <sanskarsinha1999@gmail.com>
2021-04-22 00:08:04 +0530

    function argument Commit

---
+++ function.py
@@ -0,0 +1,14 @@
+#simple function
+def my_func():
+    print("spam")
+    print("spam")
+    print("spam")
+my_func()
+
+# The example below defines a function that takes one argument
+def print_with_exclamation(word):
+    print( word + "!")
+
+print_with_exclamation("spam")
+print_with_exclamation("egg")
+print_with_exclamation("python")

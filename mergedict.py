a6ff08d5a728d852fab606856a0188e9fa858784
260734 <sanskarsinha1999@gmail.com>
2021-04-23 12:43:50 +0530

    merge two dictionary Commit

--- mergedict.py
+++ mergedict.py
@@ -1,6 +1,9 @@
-# Python script to merge two Python dictionaries
-s1 = {'qw': 45, 'er': 67}
-s2 = {'as': 23, 'df': 87}
-d = s1.copy()
-d.update(s2)
-print(d)
+# Python script to merge two dictionaries
+my_dict1={1: "blue", 2: "red", 3: "yellow"}
+my_dict2={4: "green", 5: "black"}
+print("Without Merge")
+print(my_dict1)
+print(my_dict2)
+my_dict1.update(my_dict2)
+print("After Merge")
+print(my_dict1)

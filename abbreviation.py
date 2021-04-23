8f8b33e84b2035ce3b96596e187a7cb34e499c32
260734 <sanskarsinha1999@gmail.com>
2021-04-23 11:30:05 +0530

    input abbreviation Commit

---
+++ abbreviation.py
@@ -0,0 +1,11 @@
+#program to check the user input abbreviation
+print("User Input Abbreviation")
+s=input()
+if s=='lol':
+    print("laughing out loud")
+elif s=='rofl':
+    print("rolling on the floor laughing")
+elif s=='lmk':
+    print("let me know")
+elif s=='smh':
+    print("shaking my head")
